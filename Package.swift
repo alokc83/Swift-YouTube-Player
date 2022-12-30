fd0d3ce880bed6c68449c7f3031a3a8cef12d180
Alok Choudhary <alok.c83@gmail.com>
2022-12-30 09:09:14 -0600

    Addin SPM support

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftYouTubePlayer",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "SwiftYouTubePlayer",
+            targets: ["SwiftYouTubePlayer"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftYouTubePlayer",
+            dependencies: []
+        )
+    ]
+)
+
